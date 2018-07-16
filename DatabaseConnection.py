f0e4fdf40d3fafdc632b234550afc80548599c8a
Johan <johan.27011993@gmail.com>
2018-07-16 15:07:38 -0400

    Database work 1

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -5,10 +5,14 @@ import pymysql as sql
 #                  password='test123',
 #                  db='ferna11i_cbioportal')
 
+try:
+    db = sql.connect(host='48588.us-imm-sql6.000webhost.io:3306', #mysql2.000webhost.com
+                     user='id6399047_fern_cbioportal',    #ferna11i_cb
+                     password='test123',
+                     db='id6399047_fern_cb')
 
-db = sql.connect(host='mysql2.000webhost.com',
-                 user='id6399047_fern_cbioportal',    #ferna11i_cb
-                 password='test123',
-                 db='id6399047_fern_cb')
+    cursor = db.cursor()
 
-cursor = db.cursor()
+except Exception as e:
+    print(e)
+
