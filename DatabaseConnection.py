6fae721fb935e972ad278a8e482e0bc2d51d674a
Johan <johan.27011993@gmail.com>
2018-07-16 17:18:50 -0400

    Database II

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,18 +1,28 @@
-import pymysql as sql
+import requests as req
 
-try:
-    # db = sql.connect(host='48588.us-imm-sql6.000webhost.io', #mysql2.000webhost.com
-    #                  user='id6399047_fern_cbioportal',    #ferna11i_cb
-    #                  password='test123',
-    #                  db='id6399047_fern_cb')
+# import pymysql as sql
+#
+# try:
+#     # db = sql.connect(host='48588.us-imm-sql6.000webhost.io', #mysql2.000webhost.com
+#     #                  user='id6399047_fern_cbioportal',    #ferna11i_cb
+#     #                  password='test123',
+#     #                  db='id6399047_fern_cb')
+#
+#     db = sql.connect(host='myweb2.cs.uwindsor.ca',
+#                      user='ferna11i_cb',
+#                      password='test123',
+#                      db='ferna11i_cbioportal')
+#
+#     cursor = db.cursor()
+#
+# except Exception as e:
+#     print(e)
 
-    db = sql.connect(host='myweb2.cs.uwindsor.ca',
-                     user='ferna11i_cb',
-                     password='test123',
-                     db='ferna11i_cbioportal')
+url = "http://ferna11i.myweb.cs.uwindsor.ca/CBioPortal/Services/init.php";
 
-    cursor = db.cursor()
+response = req.get(url);
 
-except Exception as e:
-    print(e)
+print(response.content);
 
+for value in dict.items(response.content):
+    print("{} ".format(value))
--- Services/init.php
+++ Services/init.php
@@ -9,4 +9,26 @@ $conn = new mysqli($hostname, $username , $password, $database);
 	echo "All good !";
  }*/
  
+ $query = "SELECT * FROM studies";
+ $result = $conn->query($query);
+ 
+ if(!$result) die ("Database access failed: " . $conn->error);
+ 
+  //$num_rows = $result->num_rows;
+  $rows = array();
+  
+/*  for ($j = 0 ; $j < $num_rows ; ++$j)
+  {
+    $result->data_seek($j);
+    $row = $result->fetch_array(MYSQLI_NUM);
+
+    //echo $row[0] . "  " . $row[1];
+	$rows[$j] = $row;
+  }	*/
+
+  while($row = $result->fetch_assoc()){
+	$rows[] = $row;  
+  }	  
+  
+  echo json_encode($rows);	
 ?>
