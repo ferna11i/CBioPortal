da8d76cf2c86db0f24afdce8cb715606e8aaabc8
Johan <johan.27011993@gmail.com>
2018-07-16 16:41:22 -0400

    Database Connect I

--- DatabaseConnection.py
+++ DatabaseConnection.py
@@ -1,15 +1,15 @@
 import pymysql as sql
 
-# db = sql.connect(host='myweb2.cs.uwindsor.ca:3306',
-#                  user='ferna11i@localhost',    #ferna11i_cb
-#                  password='test123',
-#                  db='ferna11i_cbioportal')
-
 try:
-    db = sql.connect(host='48588.us-imm-sql6.000webhost.io:3306', #mysql2.000webhost.com
-                     user='id6399047_fern_cbioportal',    #ferna11i_cb
+    # db = sql.connect(host='48588.us-imm-sql6.000webhost.io', #mysql2.000webhost.com
+    #                  user='id6399047_fern_cbioportal',    #ferna11i_cb
+    #                  password='test123',
+    #                  db='id6399047_fern_cb')
+
+    db = sql.connect(host='myweb2.cs.uwindsor.ca',
+                     user='ferna11i_cb',
                      password='test123',
-                     db='id6399047_fern_cb')
+                     db='ferna11i_cbioportal')
 
     cursor = db.cursor()
 
---
+++ Services/init.php
@@ -0,0 +1,12 @@
+<?php
+ require_once "login.php";
+ 
+$conn = new mysqli($hostname, $username , $password, $database);
+ 
+ /*if($conn->connect_error){
+	echo "Problem with connection !! ";
+ }else{
+	echo "All good !";
+ }*/
+ 
+?>
