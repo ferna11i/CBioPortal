02b6a91e130f8f75cb290aa2fbe11c84a8b79d5b
VielleichtFaith <40347792+VielleichtFaith@users.noreply.github.com>
2018-07-18 21:02:42 -0400

    updated with css and table headers

--- Services/load_data.php
+++ Services/load_data.php
@@ -1,33 +1,53 @@
+<html>
+<head>   
+   <link rel="stylesheet" href="../css/table.css">
+</head>
+<body>
 <?php
  require_once "login.php";
  
  $conn = new mysqli($hostname,$username,$password,$database);
-
  if ($conn->connect_error) die($conn->connect_error);
 	
 $query = "SELECT * from data_objects where tag = 'gleason'";
 $result = $conn->query($query);
-
 if(!$result){
 	die($conn->error);
 }
-	
- $json = $result->fetch_assoc()['data'];
-// header('Content-type: application/json');
-// echo $json;
+$json = $result->fetch_assoc()['data'];
+$data = json_decode($json,true); #json table
+
+
+$query2 = "DESCRIBE data_objects";
+$result2 = $conn->query($query);
+if(!$result){
+	die($conn->error);
+}
+      echo '<table>';
+
+
+      echo '<thead>';
+
+      foreach($data[0] as $key => $val){ //column headers
+	     echo "<th>$key</th>" ;
+	}	
+      echo '</thead>';
+
  
- $data = json_decode($json,true);
  
  foreach($data as $item){ //row
-	//echo $item['CASE_ID'] . " " . $item['CLINICAL_GLEASON']. "<br>"; 
 	
+	echo '<tr>';
 	foreach($item as $key => $val){ //column
-		echo $val . "  " ;
+	     echo "<td>$val</td>" ;
 	}	
-	echo "<br>";
+	echo "</tr>";
  }	 
- 
-// echo $data[0]['Name'] . "<br>";
-// echo $data[1]['Name'] . "<br>";
+      echo '</table>';
+      $result->close();
+      $result2->close();
+      $conn->close();
 
 ?>
+</body>
+</html>
